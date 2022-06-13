8f73b4ac64558601cb0103588ae35def00f2b3f7
Kourosh Simpkins <kouroshsimpkins@gmail.com>
2022-06-13 01:24:28 +0100

    This servo is a nightmare to work with

--- doomsday-clock.ino
+++ doomsday-clock.ino
@@ -1,27 +1,43 @@
+// All of the required libraries
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include "Adafruit_LEDBackpack.h"
+#include "FeedBackServo.h"
 
+// Defines the pin for feedback from the servo, and the control pin.
+#define FEEDBACK_SERVO_PIN 2
+#define SERVO_PIN 3
+
+// Set the feedback signal pin number
+FeedBackServo servo = FeedBackServo(FEEDBACK_SERVO_PIN);
+
+// Sets up the 7 segment display
 Adafruit_7segment matrix = Adafruit_7segment();
 
+// A Test value for testing the 7seg display against
 int test_value = 2021;
 
 void setup()
 {
-	#ifndef __AVR_ATtiny85__
-        Serial.begin(9600);
-        Serial.println("7 Segment Backpack Test");
-    #endif
+    // Sets up the 7 segment display  
+
     matrix.begin(0x70);
+
+    // Sets up the feedback servo
+    servo.setServoControl(SERVO_PIN);
+    servo.setKp(0.01);
+    Serial.begin(115200);
 }
 
 void loop()
 {
-	matrix.print(test_value, DEC);
+    servo.rotate(270, 4);
+	matrix.print(servo.Angle(), DEC);
     matrix.writeDisplay();
-    delay(150);
+    delay(1000);
 
-    matrix.print("7SEG");
+    servo.rotate(-180, 4);
+    matrix.print(servo.Angle(), DEC);
     matrix.writeDisplay();
-    delay(150);
+    delay(1000);
 }
